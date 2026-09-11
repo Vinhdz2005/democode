c883a31aa6bc87facd1ef110ad41bdc37576b754
Congvinh2005 <vanxuank69@gmail.com>
2026-09-11 22:37:05 +0700

    Update message from developer 2q

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello from Developer 2");
+        System.out.println("This is version from Developer 2");
     }
 }
