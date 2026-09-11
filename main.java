f6ef287b317f02b349cb15ff3795da7e915edf7a
Vinhdz2005 <daovanvinh100@gmail.com>
2026-09-11 15:36:04 +0000

    Update message from developer 1

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello from Vinh");
+        System.out.println("This is version from Developer 1");
     }
 }
